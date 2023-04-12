98a684dd8c4a6e3e952989b3c906543cc07703fe
Andra Helton <andrahelton@gmail.com>
2023-04-12 16:18:22 -0600

    feat: Add error handling

---
+++ app/controllers/api/api_controller.rb
@@ -0,0 +1,9 @@
+class Api::ApiController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :error_response
+  rescue_from ActiveRecord::RecordInvalid, with: :error_response
+
+  def error_response(exception)
+    error_serializer = ErrorSerializer.new(exception)
+    render json: error_serializer.error, status: error_serializer.status
+  end
+end
