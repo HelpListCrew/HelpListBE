d9538b3cf0af2b39260c2680d330f8053bb66369
Andra Helton <andrahelton@gmail.com>
2023-04-12 18:41:35 -0600

    feat: Add route and UsersController#index

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::UsersController < Api::ApiController
+	def index
+		render json: UserSerializer.new(User.all)
+	end
+
 	def show
 		render json: UserSerializer.new(User.find(params[:id]))
 	end
