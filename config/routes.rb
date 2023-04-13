b98a4d2e8c9292bb5194c70f322baaa7251e0ef9
Jade Stewart <jadekstewart3@gmail.com>
2023-04-13 16:30:32 -0600

    Create route for login user

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create, :show, :index]
       resources :wishlist_items, only: [:show, :index, :create]
+      post "/login", to: "users#login_user"
     end
   end
+
 end
