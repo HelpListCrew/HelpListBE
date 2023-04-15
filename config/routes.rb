c818788229195a0f91f22826bb60f66c1334c1d5
Andra Helton <andrahelton@gmail.com>
2023-04-14 18:17:40 -0600

    Take out comments in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   namespace :api do
     namespace :v1 do
       get "/users/find_all", to: "users/search#index"
