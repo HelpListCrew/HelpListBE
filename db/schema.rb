51cda2f51969b73386e780bfc95b38316f417c33
Andra Helton <andrahelton@gmail.com>
2023-04-18 15:44:25 -0600

    Add additional Organization seed data

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_201209) do
     t.integer "user_type", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "uid", default: "57255e32c2848391fcb4b1b00f511a"
+    t.string "uid", default: "baf69b2c9a65223eb72a2e1bb1e561"
     t.string "username"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 organization = FactoryBot.create(:organization, mission_statement: "To empower and uplift individuals, families, and communities in need through comprehensive support, compassionate care, and sustainable solutions.")
+org2 = FactoryBot.create(:organization, mission_statement: nil)
+
 organization.users.create!(email: "recipient@gmail.com", password: "123", user_type: 1)
+organization.users.create!(email: "anotherrecipient@gmail.com", username: "OceanicDreamer", password: "123", user_type: 1)
+organization.users.create!(email: "athirdrecipient@gmail.com", username: "Stargazer321" password: "123", user_type: 1)
+
+org2.users.create!(email: "afourthrecipient@gmail.com", username: "Stargazer321" password: "321", user_type: 1)
+org2.users.create!(email: "afifthrecipient@gmail.com", username: "CloudWatcher88" password: "321", user_type: 1)
+org2.users.create!(email: "asixthrecipient@gmail.com", username: "AdventureHikez" password: "321", user_type: 1)
+
 
 donor = User.create!(email: "donor@gmail.com", password: "123")
 
